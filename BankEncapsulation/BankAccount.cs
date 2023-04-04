0b94369c0ae8323d42ed48f5909120882089ce46
Autif Kamal <kautif@gmail.com>
2023-04-04 10:09:42 -0500

    added bank account class and balance methods

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double deposit) {
+            balance += deposit;
+            Console.WriteLine($"Deposited {deposit}. Balance is now {balance}");
+        }
+
+        public double GetBalance()
+        {
+            Console.WriteLine("Balance:");
+            return balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Enter 'deposit' to add funds to your account or 'balance' to see your balance");
+            string response = Console.ReadLine();
+
+            if (response == "deposit")
+            {
+                Console.WriteLine("Enter deposit amount in numbers only");
+                string deposit = Console.ReadLine();
+                account.Deposit(double.Parse(deposit));
+            }
+            else if (response == "balance")
+            {
+                Console.WriteLine(account.GetBalance());
+            }
+            else {
+                Console.WriteLine("Request not recognized");
+            }
         }
     }
 }
